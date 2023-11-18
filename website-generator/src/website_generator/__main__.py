2b09a32357b205402fbb8b02f3cf9a853d06b6e0
ahmedkhalf <ahmedkhalf567@gmail.com>
2023-11-18 17:30:21 -0500

    Read post list

--- website-generator/src/website_generator/__main__.py
+++ website-generator/src/website_generator/__main__.py
@@ -2,6 +2,7 @@
 #
 # SPDX-License-Identifier: MIT
 
+from dataclasses import dataclass
 from pathlib import Path
 
 from jinja2 import Environment, FileSystemLoader, select_autoescape
@@ -9,21 +10,33 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 CWD = Path.cwd().resolve()
 TEMPLATE_FOLDER = CWD / "templates"
+POSTS_FOLDER = CWD / "posts"
 OUT_FOLDER = CWD / "out"
 
 
+@dataclass
+class Post:
+    file: str
+
+
 def main():
+    # load template
     env = Environment(
         loader=FileSystemLoader(TEMPLATE_FOLDER),
         autoescape=select_autoescape()
     )
-
     main_template = env.get_template("main.html")
+
+    # load posts
+    posts: list[Post] = []
+    for post_path in POSTS_FOLDER.glob("*.md"):
+        posts.append(Post(file=post_path.name))
 
     # create an output directory if it does not exist
     OUT_FOLDER.mkdir(exist_ok=True)
 
-    main_page = main_template.render()
+    # render
+    main_page = main_template.render(posts=posts)
     with open(OUT_FOLDER / "main.html", "w") as f:
         f.write(main_page)
 
